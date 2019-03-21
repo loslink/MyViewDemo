f85e0b9a2b3f686cb604a07d8efabfbb73af6a89
loslink <965918077@qq.com>
2019-03-21 17:36:11 +0800

    new

--- app/src/main/java/com/loslink/myview/widget/CleanNewView.java
+++ app/src/main/java/com/loslink/myview/widget/CleanNewView.java
@@ -34,8 +34,8 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class CleanNewView extends View {
 
-    private Paint itemPaint,circleWhitePaint,circleInnerPaint,bunblePaint;
-    private long duration=3000;
+    private Paint itemPaint, circleWhitePaint, circleInnerPaint, bunblePaint;
+    private long duration = 3000;
     private Context context;
     private float canvasWidth, canvasHeight;
     private float OUTER_RADIUS = 133;
@@ -43,24 +43,24 @@ public class CleanNewView extends View {
     private float BAR_HEIGHT = 5;
     private float INNER_OUT_RADIUS = 90;
     private float INNER_IN_RADIUS = 80;
-    private float startDegree=-215, endDegree=35f;
-    private float currentDegree=startDegree;
-    private float itemDegree=5f;
-    private ValueAnimator animator,repeatAnimator;
+    private float startDegree = -215, endDegree = 35f;
+    private float currentDegree = startDegree;
+    private float itemDegree = 5f;
+    private ValueAnimator animator, repeatAnimator;
     private RectF itemRectF;
     private Path itemPath;
-    private int colorBitWidth=100,colorBitHeigth=10;
-    private Bitmap colorBitmap;
-    private float levelProgress=0f;//垃圾级别
-    private float junkFileSizeMax=400f;//垃圾分界最高
-    private boolean isStart=false;
+    private int colorBitWidth = 100, colorBitHeigth = 10;
+    private Bitmap colorBitmap, startColorBitmap;
+    private float levelProgress = 0f;//垃圾级别
+    private float junkFileSizeMax = 400f;//垃圾分界最高
+    private boolean isStart = false;
     private Runnable delayRunnable;
     private List<Bubble> bubbles = new ArrayList<>();
     private float centerWidth = 100, centerHeight = 100;
     private List<ValueAnimator> animatorList;
     private MainCleanNewView.CleanState currentCleanState;
     private float junkFileSize;
-    private int blueItemCount=6;
+    private int blueItemCount = 6;
     private Matrix shadowMatrix;
     private Bitmap shadowBitmap;
 
@@ -96,7 +96,7 @@ public class CleanNewView extends View {
         itemPaint = new Paint();
         itemPaint.setAntiAlias(true);
         itemPaint.setStyle(Paint.Style.FILL);
-        itemPaint.setShadowLayer(4f,2f,2f,Color.parseColor("#22000000"));
+        itemPaint.setShadowLayer(4f, 2f, 2f, Color.parseColor("#22000000"));
 
         circleWhitePaint = new Paint();
         circleWhitePaint.setAntiAlias(true);
@@ -115,12 +115,12 @@ public class CleanNewView extends View {
         bunblePaint.setColor(Color.WHITE);
         bunblePaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
 
-        shadowMatrix =new Matrix();
+        shadowMatrix = new Matrix();
         shadowBitmap = ((BitmapDrawable) getResources().getDrawable(R.mipmap.ic_main_clean_shadow)).getBitmap();
-        float resultW=(INNER_OUT_RADIUS+DipToPx.dipToPx(context,6))*2f;
-        float par=resultW/(float) shadowBitmap.getWidth();
-        shadowMatrix.setScale(par,par);
-        shadowMatrix.postTranslate(-resultW/2,-resultW/2);
+        float resultW = (INNER_OUT_RADIUS + DipToPx.dipToPx(context, 6)) * 2f;
+        float par = resultW / (float) shadowBitmap.getWidth();
+        shadowMatrix.setScale(par, par);
+        shadowMatrix.postTranslate(-resultW / 2, -resultW / 2);
 
     }
 
@@ -149,45 +149,47 @@ public class CleanNewView extends View {
 
     /**
      * 画内圈
+     *
      * @param canvas
      */
-    private void drowCircle(Canvas canvas){
-        float progress=(currentDegree-startDegree)/(endDegree-startDegree);
-        if(currentCleanState == MainCleanNewView.CleanState.Checking){
-            progress=junkFileSize/junkFileSizeMax;
-            if(progress>1f){
-                progress=1f;
+    private void drowCircle(Canvas canvas) {
+        float progress = (currentDegree - startDegree) / (endDegree - startDegree);
+        if (currentCleanState == MainCleanNewView.CleanState.Checking) {
+            progress = junkFileSize / junkFileSizeMax;
+            if (progress > 1f) {
+                progress = 1f;
             }
         }
 
-        canvas.drawBitmap(shadowBitmap, shadowMatrix,circleWhitePaint);
-        canvas.drawCircle(0,0,INNER_OUT_RADIUS,circleWhitePaint);
-        int pixel = getBitmapColor(progress);
+        canvas.drawBitmap(shadowBitmap, shadowMatrix, circleWhitePaint);
+        canvas.drawCircle(0, 0, INNER_OUT_RADIUS, circleWhitePaint);
+        int pixel = getStartBitmapColor(progress);
         int a = Color.alpha(pixel);
         int r = Color.red(pixel);
         int g = Color.green(pixel);
         int b = Color.blue(pixel);
-        int startColor=Color.argb(a*2/3,r,g,b);
-        LinearGradient linearGradient=new LinearGradient(-INNER_IN_RADIUS,-INNER_IN_RADIUS,
-                INNER_IN_RADIUS-DipToPx.dipToPx(context,80),INNER_IN_RADIUS-DipToPx.dipToPx(context,80),
-                startColor,
+        int startColor = Color.argb(a * 2 / 3, r, g, b);
+        LinearGradient linearGradient = new LinearGradient(-INNER_IN_RADIUS, -INNER_IN_RADIUS,
+                INNER_IN_RADIUS - DipToPx.dipToPx(context, 80), INNER_IN_RADIUS - DipToPx.dipToPx(context, 80),
                 pixel,
+                getBitmapColor(progress),
                 Shader.TileMode.CLAMP);
         circleInnerPaint.setShader(linearGradient);
         //调用saveLayer时，会生成了一个全新的bitmap，这个bitmap的大小就是我们指定的保存区域的大小，新生成的bitmap是全透明的，在调用saveLayer后所有的绘图操作都是在这个bitmap上进行的。
-        int layerID = canvas.saveLayer(-canvasWidth/2, -canvasHeight/2, canvasWidth/2, canvasHeight/2, circleInnerPaint, Canvas.ALL_SAVE_FLAG);
-        canvas.drawCircle(0,0,INNER_IN_RADIUS,circleInnerPaint);
-        linearGradient=null;
+        int layerID = canvas.saveLayer(-canvasWidth / 2, -canvasHeight / 2, canvasWidth / 2, canvasHeight / 2, circleInnerPaint, Canvas.ALL_SAVE_FLAG);
+        canvas.drawCircle(0, 0, INNER_IN_RADIUS, circleInnerPaint);
+        linearGradient = null;
         drawBunbles(canvas);
     }
 
     /**
      * 绘制小气泡
+     *
      * @param canvas
      */
-    private void drawBunbles(Canvas canvas){
+    private void drawBunbles(Canvas canvas) {
         canvas.save();
-        canvas.translate(-INNER_IN_RADIUS,-INNER_IN_RADIUS);
+        canvas.translate(-INNER_IN_RADIUS, -INNER_IN_RADIUS);
         for (Bubble bubble : bubbles) {
             if (bubble.pointF.x > 30) {
                 bunblePaint.setAlpha(bubble.alph);
@@ -199,37 +201,38 @@ public class CleanNewView extends View {
 
     /**
      * 绘制外圈小块
+     *
      * @param canvas
      */
-    private void drawOuterItems(Canvas canvas){
+    private void drawOuterItems(Canvas canvas) {
         canvas.save();
         canvas.rotate(startDegree);
-        float lastDegree=startDegree;
-        int itemIndex=0;
+        float lastDegree = startDegree;
+        int itemIndex = 0;
 
         while (true) {
-            if(lastDegree > endDegree){
+            if (lastDegree > endDegree) {
                 break;
             }
-            switch (currentCleanState){
+            switch (currentCleanState) {
                 case Analysing:
                     itemPaint.setColor(getColor(R.color.cleanItemGrayColor));
                     break;
                 case AnalyseFinish:
                     break;
                 case Checking:
-                    int currentItem=(int)((currentDegree-startDegree)/itemDegree);
-                    if(itemIndex>=(currentItem-blueItemCount) && itemIndex<=currentItem){
+                    int currentItem = (int) ((currentDegree - startDegree) / itemDegree);
+                    if (itemIndex >= (currentItem - blueItemCount) && itemIndex <= currentItem) {
                         itemPaint.setColor(Color.parseColor("#3c8ef4"));
-                    }else{
+                    } else {
                         itemPaint.setColor(getColor(R.color.cleanItemGrayColor));
                     }
                     break;
                 case CheckFinish:
-                    if(isStart && lastDegree <= currentDegree){
-                        float progress=(lastDegree-startDegree)/(endDegree-startDegree);
+                    if (isStart && lastDegree <= currentDegree) {
+                        float progress = (lastDegree - startDegree) / (endDegree - startDegree);
                         itemPaint.setColor(getBitmapColor(progress));
-                    }else{
+                    } else {
                         itemPaint.setColor(getColor(R.color.cleanItemGrayColor));
                     }
                     break;
@@ -240,15 +243,15 @@ public class CleanNewView extends View {
             canvas.drawPath(itemPath, itemPaint);
 
             canvas.rotate(itemDegree);
-            lastDegree = lastDegree+itemDegree;
+            lastDegree = lastDegree + itemDegree;
             itemIndex++;
         }
         canvas.restore();
     }
 
-    public void setCleanState(MainCleanNewView.CleanState cleanState){
-        currentCleanState=cleanState;
-        switch (cleanState){
+    public void setCleanState(MainCleanNewView.CleanState cleanState) {
+        currentCleanState = cleanState;
+        switch (cleanState) {
             case Analysing:
                 levelProgress = 0.5f;
                 break;
@@ -266,29 +269,29 @@ public class CleanNewView extends View {
         }
     }
 
-    public void setJunkFileSize(float junkFileSize){
-        this.junkFileSize=junkFileSize;
+    public void setJunkFileSize(float junkFileSize) {
+        this.junkFileSize = junkFileSize;
     }
 
     /**
      * 开始动画
      */
-    public void startAnimation(){
+    public void startAnimation() {
 
-        if(currentCleanState == MainCleanNewView.CleanState.Checking){
+        if (currentCleanState == MainCleanNewView.CleanState.Checking) {
             startRepeatAnimation();
             return;
         }
 
-        if(animator!=null && animator.isRunning()){
+        if (animator != null && animator.isRunning()) {
             animator.cancel();
         }
         cancelBunblesAnim();
-        if(delayRunnable!=null){
+        if (delayRunnable != null) {
             removeCallbacks(delayRunnable);
         }
 
-        animator=ValueAnimator.ofFloat(startDegree,endDegree);
+        animator = ValueAnimator.ofFloat(startDegree, endDegree);
 
         animator.setDuration(duration);
         animator.setInterpolator(new LinearInterpolator());
@@ -297,7 +300,7 @@ public class CleanNewView extends View {
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                currentDegree= (float) animation.getAnimatedValue();
+                currentDegree = (float) animation.getAnimatedValue();
                 postInvalidate();
             }
         });
@@ -305,29 +308,29 @@ public class CleanNewView extends View {
             @Override
             public void onAnimationEnd(Animator animation) {
                 super.onAnimationEnd(animation);
-                delayRunnable=new Runnable() {
+                delayRunnable = new Runnable() {
                     @Override
                     public void run() {
                         cancelBunblesAnim();
                     }
                 };
-                postDelayed(delayRunnable,3000);
+                postDelayed(delayRunnable, 3000);
             }
         });
         animator.start();
-        isStart=true;
-        anima((int)(duration/1000)*2);//启动气泡
+        isStart = true;
+        anima((int) (duration / 1000) * 2);//启动气泡
     }
 
-    public void startRepeatAnimation(){
+    public void startRepeatAnimation() {
 
-        if(repeatAnimator!=null && repeatAnimator.isRunning()){
+        if (repeatAnimator != null && repeatAnimator.isRunning()) {
             repeatAnimator.cancel();
         }
 
-        repeatAnimator=ValueAnimator.ofFloat(startDegree,endDegree+(itemDegree*blueItemCount));
+        repeatAnimator = ValueAnimator.ofFloat(startDegree, endDegree + (itemDegree * blueItemCount));
 
-        final long duration=1500;
+        final long duration = 1500;
         repeatAnimator.setDuration(duration);
         repeatAnimator.setInterpolator(new LinearInterpolator());
         repeatAnimator.setRepeatCount(ValueAnimator.INFINITE);
@@ -335,9 +338,9 @@ public class CleanNewView extends View {
         repeatAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                currentDegree= (float) animation.getAnimatedValue();
+                currentDegree = (float) animation.getAnimatedValue();
                 postInvalidate();
-                if(currentCleanState == MainCleanNewView.CleanState.CheckFinish){
+                if (currentCleanState == MainCleanNewView.CleanState.CheckFinish) {
                     repeatAnimator.cancel();
                     startAnimation();
                 }
@@ -347,8 +350,8 @@ public class CleanNewView extends View {
             @Override
             public void onAnimationRepeat(Animator animation) {
                 super.onAnimationRepeat(animation);
-                for(int i=0;i<duration/500;i++){
-                    startBunblesAnim(i*500);
+                for (int i = 0; i < duration / 500; i++) {
+                    startBunblesAnim(i * 500);
                 }
             }
         });
@@ -358,17 +361,17 @@ public class CleanNewView extends View {
                 super.onAnimationEnd(animation);
             }
         });
-        for(int i=0;i<duration/500;i++){
-            startBunblesAnim(i*500);
+        for (int i = 0; i < duration / 500; i++) {
+            startBunblesAnim(i * 500);
         }
         repeatAnimator.start();
-        isStart=true;
+        isStart = true;
     }
 
     private Bubble getRodmBubble() {
-        float startRadius=DipToPx.dipToPx(context,3);
-        float endRadius=DipToPx.dipToPx(context,7);
-        float radios=(float)(Math.random()*(endRadius-startRadius)+startRadius);
+        float startRadius = DipToPx.dipToPx(context, 3);
+        float endRadius = DipToPx.dipToPx(context, 7);
+        float radios = (float) (Math.random() * (endRadius - startRadius) + startRadius);
 
         float centerDX = (float) (Math.random() * centerWidth);
         float centerDY = (float) (Math.random() * centerHeight);
@@ -438,7 +441,7 @@ public class CleanNewView extends View {
     private void cancelBunblesAnim() {
         if (null != animatorList && animatorList.size() > 0) {
             for (ValueAnimator animator : animatorList) {
-                if(animator != null && animator.isRunning()){
+                if (animator != null && animator.isRunning()) {
                     animator.cancel();
                 }
                 if (null != animator) {
@@ -455,16 +458,17 @@ public class CleanNewView extends View {
     }
 
     private int getBitmapColor(float progress) {
-        if(colorBitmap==null || colorBitmap.isRecycled()){
-            colorBitmap = getColorBitmap();
+        if (colorBitmap == null || colorBitmap.isRecycled()) {
+            int[] colors = new int[]{getColor(R.color.colorList1), getColor(R.color.colorList2), getColor(R.color.colorList3), getColor(R.color.colorList4), getColor(R.color.colorList5)};
+            colorBitmap = getColorBitmap(colors);
         }
-        float x, y=colorBitHeigth/2;
-        if(progress<0){
-            progress=0;
-        }else if(progress>1){
-            progress=1;
+        float x, y = colorBitHeigth / 2;
+        if (progress < 0) {
+            progress = 0;
+        } else if (progress > 1) {
+            progress = 1;
         }
-        x=colorBitWidth*progress*levelProgress;
+        x = colorBitWidth * progress * levelProgress;
         // 为了防止越界
         int intX = (int) x;
         int intY = (int) y;
@@ -477,39 +481,63 @@ public class CleanNewView extends View {
         return colorBitmap.getPixel(intX, intY);
     }
 
-    private Bitmap getColorBitmap() {
-        Bitmap mColorAreaBmp = null;
-        if (mColorAreaBmp == null) {
-            int width=colorBitWidth,height=colorBitHeigth;
-            Paint leftPaint = new Paint();
-            leftPaint.setStrokeWidth(1);
-            mColorAreaBmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-            Canvas canvas = new Canvas(mColorAreaBmp);
-            int bitmapWidth = mColorAreaBmp.getWidth();
-            int bitmapHeight = mColorAreaBmp.getHeight();
-            int[] colors = new int[]{getColor(R.color.colorList1), getColor(R.color.colorList2), getColor(R.color.colorList3), getColor(R.color.colorList4), getColor(R.color.colorList5)};
-            Shader leftShader = new LinearGradient(0, bitmapHeight / 2, bitmapWidth, bitmapHeight / 2, colors, null, Shader.TileMode.CLAMP);
-            ComposeShader shader = new ComposeShader(leftShader, leftShader, PorterDuff.Mode.SCREEN);
-
-            leftPaint.setShader(leftShader);
-            canvas.drawRect(0, 0, width, height, leftPaint);
+    private int getStartBitmapColor(float progress) {
+        if (startColorBitmap == null || startColorBitmap.isRecycled()) {
+            int[] colors = new int[]{getColor(R.color.startColorList1), getColor(R.color.startColorList2), getColor(R.color.startColorList3), getColor(R.color.startColorList4), getColor(R.color.startColorList5)};
+            startColorBitmap = getColorBitmap(colors);
         }
+        float x, y = colorBitHeigth / 2;
+        if (progress < 0) {
+            progress = 0;
+        } else if (progress > 1) {
+            progress = 1;
+        }
+        x = colorBitWidth * progress * levelProgress;
+        // 为了防止越界
+        int intX = (int) x;
+        int intY = (int) y;
+        if (intX >= startColorBitmap.getWidth()) {
+            intX = startColorBitmap.getWidth() - 1;
+        }
+        if (intY >= startColorBitmap.getHeight()) {
+            intY = startColorBitmap.getHeight() - 1;
+        }
+        return startColorBitmap.getPixel(intX, intY);
+    }
+
+    private Bitmap getColorBitmap(int[] colors) {
+        Bitmap mColorAreaBmp = null;
+        int width = colorBitWidth, height = colorBitHeigth;
+        Paint leftPaint = new Paint();
+        leftPaint.setStrokeWidth(1);
+        mColorAreaBmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(mColorAreaBmp);
+        int bitmapWidth = mColorAreaBmp.getWidth();
+        int bitmapHeight = mColorAreaBmp.getHeight();
+        Shader leftShader = new LinearGradient(0, bitmapHeight / 2, bitmapWidth, bitmapHeight / 2, colors, null, Shader.TileMode.CLAMP);
+        ComposeShader shader = new ComposeShader(leftShader, leftShader, PorterDuff.Mode.SCREEN);
+
+        leftPaint.setShader(leftShader);
+        canvas.drawRect(0, 0, width, height, leftPaint);
         return mColorAreaBmp;
     }
 
-    private int getColor(@ColorRes int color){
+    private int getColor(@ColorRes int color) {
         return context.getResources().getColor(color);
     }
 
-    public void destroy(){
-        if(animator!=null && animator.isRunning()){
+    public void destroy() {
+        if (animator != null && animator.isRunning()) {
             animator.cancel();
         }
-        if(repeatAnimator!=null && repeatAnimator.isRunning()){
+        if (repeatAnimator != null && repeatAnimator.isRunning()) {
             repeatAnimator.cancel();
         }
-        if(colorBitmap!=null && !colorBitmap.isRecycled()){
+        if (colorBitmap != null && !colorBitmap.isRecycled()) {
             colorBitmap.recycle();
+        }
+        if (startColorBitmap != null && !startColorBitmap.isRecycled()) {
+            startColorBitmap.recycle();
         }
         cancelBunblesAnim();
     }
